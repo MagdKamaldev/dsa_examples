a09f3985c9b39120f288a30f121cd1b50bc77d9b
MagdKamaldev <magddevf@gmail.com>
2023-09-10 09:52:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+      Map m = new HashMap();
+      m.put("Magd",1);
+      m.put("Megood",5);
+      System.out.println(m);
+    }
+}
