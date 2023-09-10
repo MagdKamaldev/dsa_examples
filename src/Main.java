c4572adf4d154fcf0ae55a3e8e92f532eac6483f
MagdKamaldev <magddevf@gmail.com>
2023-09-10 10:16:38 +0300

    last update od 10/9

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-      Map m = new HashMap();
-      m.put("Magd",1);
-      m.put("Megood",5);
-      System.out.println(m);
+
+      System.out.println("m");
     }
 }
