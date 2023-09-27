65c168162a22dda8fd61a621d71dd81b36e5a410
MagdKamaldev <magddevf@gmail.com>
2023-09-27 10:38:51 +0300

    default

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int number1 = in.nextInt();
+        int [][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(Arrays.toString(arr));
     }
 
 
