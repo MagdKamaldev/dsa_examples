b5fe2789ef7956149670f9a2427bace5a637b830
MagdKamaldev <magddevf@gmail.com>
2023-09-27 13:57:50 +0300

    default

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int [][] arr = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
-        };
-        System.out.println(Arrays.toString(arr));
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(5);
+        System.out.println(arrayList);
     }
 
 
