1012c40056d8ef08e3ea2615b9913f2eea744542
MagdKamaldev <magddevf@gmail.com>
2023-09-28 20:07:53 +0300

    default

---
+++ src/com/magd/array_from_perm.java
@@ -0,0 +1,18 @@
+package com.magd;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class array_from_perm {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(buildArray(new int[]{0,2,1,5,3,4})));
+    }
+    public static int[] buildArray(int[] nums) {
+        int [] ans =  new int [nums.length];
+        for(int i = 0 ;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
