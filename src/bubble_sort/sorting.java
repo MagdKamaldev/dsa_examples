9b3a43f849e4fcd8aebdd7aed51ec28a8c348fb0
MagdKamaldev <magddevf@gmail.com>
2023-12-19 18:02:41 +0200

    selection sort

--- src/bubble_sort/sorting.java
+++ src/bubble_sort/sorting.java
@@ -2,11 +2,33 @@ package bubble_sort;
 
 import java.util.Arrays;
 
-public class buuble_sort {
+public class sorting {
     public static void main(String[] args) {
         int [] arr = new int[]{1,2,5,4,6,10,9,7,8};
-        bubble(arr);
+        selection(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr,0,last);
+            sawp(arr,maxIndex,last);
+        }
+    }
+    static void sawp(int [] arr, int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr [second] = temp;
+    }
+    static  int  getMaxIndex(int[] arr,int start, int end){
+      int max = start;
+        for (int i = start; i <= end; i++) {
+         if(arr[max]<arr[i]){
+             max = i;
+         }
+        }
+        return max;
     }
     static void bubble (int [] arr){
         boolean swapped;
