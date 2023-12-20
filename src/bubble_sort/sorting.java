cf23ce4118ad18d091bf5eca9916b9d3ff7d0fa6
MagdKamaldev <magddevf@gmail.com>
2023-12-20 20:39:04 +0200

    insertion sort

--- src/bubble_sort/sorting.java
+++ src/bubble_sort/sorting.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class sorting {
     public static void main(String[] args) {
         int [] arr = new int[]{1,2,5,4,6,10,9,7,8};
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -13,13 +13,26 @@ public class sorting {
         for (int i = 0; i < arr.length; i++) {
             int last = arr.length-i-1;
             int maxIndex = getMaxIndex(arr,0,last);
-            sawp(arr,maxIndex,last);
+            swap(arr,maxIndex,last);
         }
     }
-    static void sawp(int [] arr, int first,int second){
+    static void swap(int [] arr,int first ,int second){
         int temp = arr[first];
         arr[first] = arr[second];
         arr [second] = temp;
+    }
+
+    static void insertion(int [] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+
+                }else{
+                    break;
+                }
+            }
+        }
     }
     static  int  getMaxIndex(int[] arr,int start, int end){
       int max = start;
