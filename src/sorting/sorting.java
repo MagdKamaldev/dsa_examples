2c38efe9ff7ec4deb66ade3a947800d548b8ed80
MagdKamaldev <magddevf@gmail.com>
2023-12-25 20:42:15 +0200

    insertion sort

--- src/sorting/sorting.java
+++ src/sorting/sorting.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class sorting {
     public static void main(String[] args) {
         int [] arr = new int[]{1,2,5,4,6,10,9,7,8};
-        insertion(arr);
+        bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -48,9 +48,7 @@ public class sorting {
             swapped=false;
             for (int j = 1; j < arr.length-i; j++) {
                 if(arr[j]<arr[j-1]){
-                    int temp = arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
+                    swap(arr,j,j-1);
                     swapped= true;
                 }
             }
